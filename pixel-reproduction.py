2cfc32fb261692174ef4a032d150de2f3af430c1
Shimon Shai Berkovich <shimonbe@shimonsh-mbp.local>
2020-01-23 17:05:16 +0200

    working stage

--- pixel-reproduction.py
+++ pixel-reproduction.py
@@ -1,27 +1,37 @@
 import argparse
-import numpy
+# import numpy
 import time
 import math
 import random
 from multiprocessing import Pool
 from abc import ABC, abstractmethod
 import colorsys
+from enum import Enum
 
 MIN_PIXEL_RANGE = 0
 MAX_PIXEL_RANGE = 255
-LIFESPAN = 7
+LIFESPAN = 8
 DEFAULT_THRESHOLD = 4
-MIN_GENERATION_FOR_REPRODUCTION = 2
-MAX_GENERATION_FOR_REPRODUCTION = 5
+MIN_GENERATION_FOR_REPRODUCTION = 3
+MAX_GENERATION_FOR_REPRODUCTION = 6
+
+
+class ColorSchemes(Enum):
+    RGB = 'RGB'
+    HSL = 'HSL'
+    HSV = 'HSV'
+    CMYK = 'CMYK'
+
 
 
 class Pixel(ABC):
 
     threshold_to_reproduce = DEFAULT_THRESHOLD
-    dominance_dict = {}
+    dominance_dict = {ColorSchemes.RGB.value: True, ColorSchemes.HSL.value: False, ColorSchemes.HSV.value: True,
+                      ColorSchemes.CMYK.value: False}
 
     def __init__(self):
-        self.generation = 0
+        self.generation = 1
         self.rgb_representation = self.asRGB()
         self.mate = None
 
@@ -32,6 +42,10 @@ class Pixel(ABC):
     @abstractmethod
     def asRGB(self):
         pass
+
+    @classmethod
+    def set_dominance(cls, dominance_dict):
+        cls.dominance_dict = dominance_dict
 
     def get_generation(self):
         return self.generation
@@ -54,7 +68,7 @@ class Pixel(ABC):
         first_values = first.asRGB()
         second_values = second.asRGB()
 
-        return (first_values[0] + second_values[0]) // 2, (first_values[1] + second_values[1]) // 2, \
+        return (first_values[0] + second_values[0]) // 2, (first_values[1] + second_values[1]) // 2,\
                (first_values[2] + second_values[2]) // 2
 
     @staticmethod
@@ -76,22 +90,20 @@ class Pixel(ABC):
                 child_type = first_type if rnd_choice == 0 else second_type
         r, g, b = Pixel.rgb_average(couple[0], couple[1])
 
-        if child_type == 'RGB':
+        if child_type == ColorSchemes.RGB.value:
             return RGB((r, g, b))
-        elif child_type == 'HSL':
-            r, g, b = r / MAX_PIXEL_RANGE, g // MAX_PIXEL_RANGE, b // MAX_PIXEL_RANGE
+        elif child_type == ColorSchemes.HSL.value:
+            r, g, b = r / MAX_PIXEL_RANGE, g / MAX_PIXEL_RANGE, b / MAX_PIXEL_RANGE
             return HSL(HSL.from_RGB(r, g, b))
-        elif child_type == 'HSV':
-            r, g, b = r / MAX_PIXEL_RANGE, g // MAX_PIXEL_RANGE, b // MAX_PIXEL_RANGE
+        elif child_type == ColorSchemes.HSV.value:
+            r, g, b = r / MAX_PIXEL_RANGE, g / MAX_PIXEL_RANGE, b / MAX_PIXEL_RANGE
             return HSV(HSV.from_RGB(r, g, b))
-        elif child_type == 'CMYK':
-            r, g, b = r / MAX_PIXEL_RANGE, g // MAX_PIXEL_RANGE, b // MAX_PIXEL_RANGE
+        elif child_type == ColorSchemes.CMYK.value:
             return CMYK(CMYK.from_RGB(r, g, b))
 
 
 class RGB(Pixel):
-
-    options = ['RGB', 'HSV', 'CMYK']
+    options = [ColorSchemes.RGB.value, ColorSchemes.HSV.value, ColorSchemes.CMYK.value]
 
     def __init__(self, scheme):
         self.first = scheme[0]
@@ -100,11 +112,10 @@ class RGB(Pixel):
         super().__init__()
 
     def __repr__(self):
-        return '(Red= {0}, Green= {1}, Blue= {2})'.format(self.first, self.second, self.third)
+        return 'RGB(Red= {0}, Green= {1}, Blue= {2})'.format(self.first, self.second, self.third)
 
     def asRGB(self):
         return self.get_values()
-
 
     def get_options(self):
         return RGB.options
@@ -114,8 +125,7 @@ class RGB(Pixel):
 
 
 class HSL(Pixel):
-
-    options = ['HSL', 'HSV']
+    options = [ColorSchemes.HSV.value, ColorSchemes.HSL.value]
 
     def __init__(self, scheme):
         self.first = scheme[0]
@@ -124,7 +134,7 @@ class HSL(Pixel):
         super().__init__()
 
     def __repr__(self):
-        return '(Hue= {0}°, Saturation= {1}, Lightness= {2})'.format(self.first, self.second, self.third)
+        return 'HSL(Hue= {0}°, Saturation= {1}, Lightness= {2})'.format(self.first, self.second, self.third)
 
     def asRGB(self):
         h, s, l = self.get_values()
@@ -132,8 +142,9 @@ class HSL(Pixel):
 
     @staticmethod
     def from_RGB(r, g, b):
-        h, l, s = tuple(round(i * 255) for i in colorsys.rgb_to_hls(r, g, b))
-        return h, s, l
+        r, g, b = r / 255, g / 255, b / 255
+        h, l, s = colorsys.rgb_to_hls(r, g, b)
+        return round(h, 2), round(s, 2), round(l, 2)
 
 
     def get_options(self):
@@ -144,8 +155,7 @@ class HSL(Pixel):
 
 
 class HSV(Pixel):
-
-    options = ['HSV', 'RGB', 'CMYK', 'HSL']
+    options = [ColorSchemes.RGB.value, ColorSchemes.HSV.value, ColorSchemes.HSL.value, ColorSchemes.CMYK.value]
 
     def __init__(self, scheme):
         self.first = scheme[0]
@@ -155,7 +165,7 @@ class HSV(Pixel):
 
 
     def __repr__(self):
-        return '(Hue= {0}°, Saturation= {1}, Value= {2})'.format(self.first, self.second, self.third)
+        return 'HSV(Hue= {0}°, Saturation= {1}, Value= {2})'.format(self.first, self.second, self.third)
 
     def asRGB(self):
         h, s, v = self.get_values()
@@ -163,7 +173,9 @@ class HSV(Pixel):
 
     @staticmethod
     def from_RGB(r, g, b):
-        return tuple(round(i * 255) for i in colorsys.rgb_to_hsv(r, g, b))
+        r, g, b = r / MAX_PIXEL_RANGE, g / MAX_PIXEL_RANGE, b / MAX_PIXEL_RANGE
+        h, s, v = colorsys.rgb_to_hsv(r, g, b)
+        return round(h, 2), round(s, 2), round(v, 2)
 
     def get_options(self):
         return HSV.options
@@ -174,7 +186,7 @@ class HSV(Pixel):
 
 class CMYK(Pixel):
 
-    options = ['CMYK',  'HSV', 'RGB']
+    options = [ColorSchemes.RGB.value, ColorSchemes.HSV.value, ColorSchemes.CMYK.value]
 
     def __init__(self, scheme):
         self.first = scheme[0]
@@ -185,7 +197,7 @@ class CMYK(Pixel):
 
 
     def __repr__(self):
-        return '(Cyan= {0}°, Magenta= {1}, Yellow= {2}, Black= {3})'.format(self.first, self.second, self.third,
+        return 'CMYK(Cyan= {0}, Magenta= {1}, Yellow= {2}, Black= {3})'.format(self.first, self.second, self.third,
                                                                            self.forth)
 
     def get_values(self):
@@ -193,17 +205,20 @@ class CMYK(Pixel):
 
     def asRGB(self):
         cyan, magenta, yellow, black = self.get_values()
-        r = 255 * (1 - cyan) * (1 - black)
-        g = 255 * (1 - magenta) * (1 - black)
-        b = 255 * (1 - yellow) * (1 - black)
+
+        r = MAX_PIXEL_RANGE * (MAX_PIXEL_RANGE - cyan) // MAX_PIXEL_RANGE * (MAX_PIXEL_RANGE - black) // MAX_PIXEL_RANGE
+        g = MAX_PIXEL_RANGE * (MAX_PIXEL_RANGE - magenta) // MAX_PIXEL_RANGE * (MAX_PIXEL_RANGE - black)\
+            // MAX_PIXEL_RANGE
+        b = MAX_PIXEL_RANGE * (MAX_PIXEL_RANGE - yellow) // MAX_PIXEL_RANGE * (MAX_PIXEL_RANGE - black)\
+            // MAX_PIXEL_RANGE
 
         return r, g, b
 
     @staticmethod
     def from_RGB(r, g, b):
-        r_tag = r // 255
-        g_tag = g // 255
-        b_tag = b // 255
+        r_tag = r // MAX_PIXEL_RANGE
+        g_tag = g // MAX_PIXEL_RANGE
+        b_tag = b // MAX_PIXEL_RANGE
 
         k = 1 - max(r_tag, g_tag, b_tag)
         c = (1 - r_tag - k) // (1 - k)
@@ -224,13 +239,13 @@ class Population:
         self.community = {}
         for i in range(initial_amount):
             pixel_type = pixels[i][0]
-            if pixel_type == 'RGB':
+            if pixel_type == ColorSchemes.RGB.value:
                 self.community[i] = RGB(pixels[i][1:])
-            elif pixel_type == 'HSL':
+            elif pixel_type == ColorSchemes.HSL.value:
                 self.community[i] = HSL(pixels[i][1:])
-            elif pixel_type == 'HSV':
+            elif pixel_type == ColorSchemes.HSV.value:
                 self.community[i] = HSV(pixels[i][1:])
-            elif pixel_type == 'CMYK':
+            elif pixel_type == ColorSchemes.CMYK.value:
                 self.community[i] = CMYK(pixels[i][1:])
             Population.increment_id()
 
@@ -253,9 +268,11 @@ class Population:
         Population.available_id += 1
 
     def population_dilution(self):
+        new_population = {}
         for index, pixel in self.community.items():
-            if pixel.get_generation() >= LIFESPAN:
-                self.community.pop(index)
+            if pixel.get_generation() <= LIFESPAN:
+                new_population[index] = pixel
+        self.community = new_population
 
     def population_expand(self, offsprings):
         for offspring in offsprings:
@@ -272,7 +289,7 @@ class Reproduction:
         print(self)
 
     def __repr__(self):
-        return 'Stage {0}:\n{1}\n'.format(self.get_cur_stage(), self.population)
+        return 'After stage {0}:\n{1}\n'.format(self.get_cur_stage(), self.population)
 
     def increment_stage(self):
         self.stage += 1
@@ -284,27 +301,33 @@ class Reproduction:
         couples = []
         couples_ids = []
         cur_population = self.population.get_community()
+        pixels_indexes_capable_of_reproducing = []
         for id, pixel in cur_population.items():
-            if id in couples_ids or pixel.get_generation() < MIN_GENERATION_FOR_REPRODUCTION :
+            pixel_gen = pixel.get_generation()
+            if MIN_GENERATION_FOR_REPRODUCTION <= pixel_gen <= MAX_GENERATION_FOR_REPRODUCTION:
+                pixels_indexes_capable_of_reproducing.append(id)
+
+        for id in pixels_indexes_capable_of_reproducing:
+            if id in couples_ids:
                 continue
-            possible_mate = pixel.get_mate()
+            possible_mate = cur_population[id].get_mate()
             if possible_mate and possible_mate in cur_population:
-                couples.append((pixel, cur_population[possible_mate]))
+                couples.append((cur_population[id], cur_population[possible_mate]))
                 couples_ids.extend([id, possible_mate])
             else:
-                options_types = pixel.get_options()
+                options_types = cur_population[id].get_options()
                 best_option_pixel_id = None
                 best_option_pixel_value = 0
 
-                for pair_id, pair_pixel in cur_population.items():
-                    if pair_id == id or pair_id in couples_ids or type(pair_pixel).__name__ not in options_types:
+                for pair_id in pixels_indexes_capable_of_reproducing:
+                    if pair_id == id or pair_id in couples_ids or type(cur_population[pair_id]).__name__ not in options_types:
                         continue
-                    square_distance = Pixel.squares_distance(pixel, pair_pixel)
+                    square_distance = Pixel.squares_distance(cur_population[id], cur_population[pair_id])
                     if square_distance > Pixel.threshold_to_reproduce and square_distance > best_option_pixel_value:
                         best_option_pixel_id = pair_id
                 if best_option_pixel_id:
                     couples_ids.extend([id, best_option_pixel_id])
-                    couples.append((pixel, cur_population[best_option_pixel_id]))
+                    couples.append((cur_population[id], cur_population[best_option_pixel_id]))
         p = Pool()
         offspring_pixels = p.map(Pixel.reproduce_couple, couples)
         self.population.population_expand(offspring_pixels)
@@ -325,13 +348,10 @@ class Reproduction:
 
 def main():
     dominance_dict = {'RGB': True, 'HSL': False, 'HSV': True, 'CMYK': False}
-    pixels = [['RGB', 14, 240, 100],
-             ['HSL', 0.2, 0.1, 0.9],
-             ['HSV', 0.4, 0.2, 0.7],
-             ['CMYK', 120, 56, 200, 12]]
-    Pixel.dominance_dict = dominance_dict
+    pixels = [['RGB', 14, 240, 100], ['HSL', 0.2, 0.1, 0.9], ['HSV', 0.4, 0.2, 0.7], ['CMYK', 120, 56, 200, 12]]
+    Pixel.set_dominance(dominance_dict)
     population = Population(pixels)
-    Reproduction(population, 1, 5).reproduce()
+    Reproduction(population, 1, 10).reproduce()
 
 
 
